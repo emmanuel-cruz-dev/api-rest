6365206c052b8af9a1b63d1f82abd6983e33807d
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-05-19 17:09:55 -0300

    feat: conectando controllers a db

--- src/services/workoutService.js
+++ src/services/workoutService.js
@@ -1,15 +1,22 @@
+const Workout = require("../database/Workout");
+
 const getAllWorkouts = () => {
-  return;
+  const allWorkouts = Workout.getAllWorkouts();
+  return allWorkouts;
 };
+
 const getOneWorkout = () => {
   return;
 };
+
 const createNewWorkout = () => {
   return;
 };
+
 const updateOneWorkout = () => {
   return;
 };
+
 const deleteOneWorkout = () => {
   return;
 };
