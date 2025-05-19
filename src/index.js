68a70db482e0cd607c244ff196f220bd71103e84
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-05-19 16:28:55 -0300

    feat: agregando workoutRoutes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 const express = require("express");
+const v1Router = require("./v1/routes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello World!</h1>");
+  res.send(`
+    <h1>Hello desde Home</h1>
+    <p>Visita la api en este <a href="http://localhost:3000/api/v1">link</a>.</p>
+    `);
 });
+app.use("/api/v1", v1Router);
 
 app.listen(PORT, () => {
   console.log(`🚀 Server listening on port http://localhost:${PORT}`);
---
+++ src/v1/routes/workoutRoutes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.send("Get all workouts");
+});
+
+router.get("/:workoutId", (req, res) => {
+  res.send(`Get all workouts ${req.params.workoutId}`);
+});
