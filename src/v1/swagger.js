34f5e9f699860574d9b6ba928ed141ad95e6a25f
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-05-21 10:44:15 -0300

    feat: description agregado a options en swagger.js

--- src/v1/swagger.js
+++ src/v1/swagger.js
@@ -5,7 +5,11 @@ const swaggerUi = require("swagger-ui-express");
 const options = {
   definition: {
     openapi: "3.0.0",
-    info: { title: "Crossfit WOD API", version: "1.0.0" },
+    info: {
+      title: "Crossfit WOD API",
+      description: "API de prueba",
+      version: "1.0.0",
+    },
   },
   apis: ["src/v1/routes/workoutRoutes.js", "src/database/Workout.js"],
 };
