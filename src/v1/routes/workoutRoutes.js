749d6cfe173b3c0ea6c7ef7ef48006e0baf0479e
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-05-21 08:16:50 -0300

    fix: actualización de package.json, cache eliminado de workoutRoutes

--- src/v1/routes/workoutRoutes.js
+++ src/v1/routes/workoutRoutes.js
@@ -1,10 +1,8 @@
 const express = require("express");
-const apicache = require("apicache");
 const workoutController = require("../../controllers/workoutController");
 const recordController = require("../../controllers/recordController");
 
 const router = express.Router();
-const cache = apicache.middleware;
 
 router
   .get("/", workoutController.getAllWorkouts)
